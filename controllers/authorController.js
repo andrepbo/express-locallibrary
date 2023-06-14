8ab2e9c51639521ffcd9ebfd03721b50bec1a547
André Oliveira <andre.pbo@gmail.com>
2023-06-14 20:16:05 -0300

    implement author detail page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,3 +1,4 @@
+const Book = require("../models/book");
 const Author = require("../models/author");
 const asyncHandler = require("express-async-handler");
 
@@ -12,7 +13,24 @@ exports.authorList = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific Author.
 exports.authorDetail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
+  // Get details of author and all their books (in parallel)
+  const [author, allBooksByAuthor] = await Promise.all([
+    Author.findById(req.params.id).exec(),
+    Book.find({ author: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (author === null) {
+    // No results.
+    const err = new Error("Author not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("authorDetail", {
+    title: "Author Detail",
+    author: author,
+    authorBooks: allBooksByAuthor,
+  });
 });
 
 // Display Author create form on GET.
