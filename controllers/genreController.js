f9fec7b65836d313e99f3b3f9d2d1098e1ef89fc
André Oliveira <andre.pbo@gmail.com>
2023-06-14 09:35:41 -0300

    implement genre list page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -3,7 +3,11 @@ const asyncHandler = require("express-async-handler");
 
 // Display list of all Genre.
 exports.genreList = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre list");
+  const allGenre = await Genre.find().sort({ name: 1 }).exec();
+  res.render("genreList", {
+    title: "Genre List",
+    genreList: allGenre,
+  });
 });
 
 // Display detail page for a specific Genre.
